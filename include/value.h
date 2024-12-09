20cc77872c8d2f08fd0cd0b99183ac654070f78c
Matt Plastow <dfulcrum@gmail.com>
2024-12-09 15:19:39 -0500

    Through chapter 18 end

--- include/value.h
+++ include/value.h
@@ -5,14 +5,38 @@
 
 #include "common.h"
 
-// In the Lox language, all numbers use floating-point representation (for now)
-typedef double Value;
+typedef enum {
+    VAL_BOOL,
+    VAL_NIL,
+    VAL_NUMBER
+} ValueType;
+
+typedef struct {
+    ValueType type;
+    union {
+        bool boolean;
+        double number;
+    } as;
+} Value;
+
+#define IS_BOOL(value) ((value).type == VAL_BOOL)
+#define IS_NIL(value) ((value).type == VAL_NIL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+
+#define AS_BOOL(value) ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
+
+#define BOOL_VAL(value) ((Value) { VAL_BOOL, { .boolean = value } })
+#define NIL_VAL ((Value) { VAL_NIL, { .number = 0 } })
+#define NUMBER_VAL(value) ((Value) { VAL_NUMBER, { .number = value } })
 
 typedef struct {
     int capacity;
     int count;
     Value* values;
 } ValueArray;
+
+bool valuesEqual(Value a, Value b);
 
 // ValueArray memory management functions
 // Initializes a new ValueArray by setting the count and capacity to 0
