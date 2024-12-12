a39855afa0f9d433026b8544b3703356e938661e
Matt Plastow <dfulcrum@gmail.com>
2024-12-11 23:33:13 -0500

    Updated asan_compile.sh

--- asan_compile.sh
+++ asan_compile.sh
@@ -4,7 +4,9 @@ zig cc \
     src/compiler.c \
     src/debug.c \
     src/memory.c \
+    src/object.c \
     src/scanner.c \
+    src/table.c \ 
     src/value.c \
     src/vm.c \
     -g -Og -Iinclude -L/usr/local/gcc-14.2.0/lib64/ -lasan -fsanitize=address \
